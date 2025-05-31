f7a88a20976a7393b979f9253a29dec3ccf4d576
Zhuk85 <zhurilo@list.ru>
2025-05-31 10:47:10 +0500

    gemini

--- stickers/asgi.py
+++ stickers/asgi.py
@@ -2,12 +2,17 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import stickers.routing
+import stickers.routing  # Убедитесь, что этот импорт корректен и файл routing.py существует в приложении stickers
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+# Устанавливаем переменную окружения для настроек Django
+# Замените 'your_project_name.settings' на 'stickers_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stickers_project.settings')
+
+# Получаем стандартное ASGI-приложение Django для HTTP-запросов
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,  # Используем полученное выше приложение для HTTP
     "websocket": AuthMiddlewareStack(
         URLRouter(
             stickers.routing.websocket_urlpatterns
